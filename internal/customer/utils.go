4549d6b9d3d8c8599212b821e2901be388e5efef
Lucas Daniel <lucasdaniel.geo@gmail.com>
2024-02-13 19:38:03 -0300

    feat: add parser utils to customer fields
    - parse NULL strings to nil
    - parse  empty strings to nil
    - return valid strings
    - parse booleans
    - parse float64

---
+++ internal/customer/utils.go
@@ -0,0 +1,39 @@
+package customer
+
+import (
+	"strconv"
+	"strings"
+)
+
+func ParseNullString(value string) *string {
+	if value == "NULL" || value == "" {
+		return nil
+	}
+	return &value
+}
+
+func ParseBool(value string) (*bool, error) {
+	if value == "NULL" {
+		return nil, nil
+	}
+
+	boolValue, err := strconv.ParseBool(value)
+	if err != nil {
+		return nil, err
+	}
+
+	return &boolValue, nil
+}
+
+func ParseFloat64(value string) (*float64, error) {
+	if value == "NULL" {
+		return nil, nil
+	}
+	value = strings.ReplaceAll(value, ",", ".")
+	floatValue, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	return &floatValue, nil
+}
