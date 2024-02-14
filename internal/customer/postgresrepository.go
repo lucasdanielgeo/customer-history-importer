9eb5e19e606c0910477e4a45aadd759405505828
Lucas Daniel <lucasdaniel.geo@gmail.com>
2024-02-13 21:06:37 -0300

    feat: add PostgreSQL repository for customer history
    - Created CustomerHistoryDBRepository to handle saving customer history to PostgreSQL database
    - Implemented SaveBatch method to insert customer history records in batches
    - Utilized prepared statements and transactions for efficient and reliable database operations
    - Added NewPostgresCustomerHistoryRepository function to initialize the repository with a PostgreSQL database connection

---
+++ internal/customer/postgresrepository.go
@@ -0,0 +1,51 @@
+package customer
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type CustomerHistoryDBRepository struct {
+	db *sql.DB
+}
+
+func NewPostgresCustomerHistoryRepository(db *sql.DB) CustomerHistoryRepository {
+	return CustomerHistoryDBRepository{db: db}
+}
+
+func (r CustomerHistoryDBRepository) SaveBatch(customers []CustomerHistory) error {
+	query := `
+        INSERT INTO customers_purchase_history (
+            cpf, private, incomplete, last_purchase_date, average_ticket, 
+            last_purchase_ticket, most_frequent_store, last_purchase_store
+        )
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+    `
+	tx, err := r.db.Begin()
+	if err != nil {
+		return fmt.Errorf("error beginning transaction: %w", err)
+	}
+	defer tx.Rollback()
+
+	stmt, err := tx.Prepare(query)
+	if err != nil {
+		return fmt.Errorf("error preparing SQL statement: %w", err)
+	}
+	defer stmt.Close()
+
+	for _, c := range customers {
+		_, err = stmt.Exec(
+			c.CPF, c.Private, c.Incomplete, c.LastPurchaseDate, c.AverageTicket,
+			c.LastPurchaseTicket, c.MostFrequentStore, c.LastPurchaseStore,
+		)
+		if err != nil {
+			return fmt.Errorf("error executing SQL statement: %w", err)
+		}
+	}
+
+	if err = tx.Commit(); err != nil {
+		return fmt.Errorf("error committing tx: %w", err)
+	}
+
+	return nil
+}
