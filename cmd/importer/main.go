69186e26bd69da73b7969c2d8dd6f8a4460863bc
Lucas Daniel <lucasdaniel.geo@gmail.com>
2024-02-14 09:17:57 -0300

    feat: implement import process with Run function
    - Created Run function to execute the import process
    - Initialized database connection using db package
    - Configured customer history repository
    - Utilized localfs package to read customer history data from a file
    - Read customer history data from the file using the customer service
    - Implemented error handling for file reading and database operations
    - Saved customer history data to the database using the customer service

--- cmd/importer/main.go
+++ cmd/importer/main.go
@@ -9,6 +9,13 @@ import (
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/lucasdanielgeo/customer-history-importer/internal/customer"
+	"github.com/lucasdanielgeo/customer-history-importer/internal/infra/db"
+	"github.com/lucasdanielgeo/customer-history-importer/internal/infra/localfs"
+)
+
+const (
+	file = "./data/base_teste.txt"
 )
 
 func init() {
@@ -40,4 +47,36 @@ func main() {
 	}
 }
 
-func Run() {}
+func Run() {
+	db, err := db.InitDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	log.Println("[INFO] Connected to PostgreSQL database")
+
+	postgresCustomerHistoryRepository := customer.NewPostgresCustomerHistoryRepository(db)
+
+	scanner, file, err := localfs.NewFileScanner(file)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	customerService := customer.NewCustomerService(postgresCustomerHistoryRepository, scanner)
+
+	log.Println("[INFO] Reading file")
+	customers, err := customerService.ReadLines()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("[INFO] Saving on DB")
+	if err := customerService.SaveOnDB(customers); err != nil {
+		panic(err)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("error reading the file:", err)
+	}
+}
