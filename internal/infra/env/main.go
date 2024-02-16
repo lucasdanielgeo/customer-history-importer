30089186b3c504576fee03f521ac49c0001b9a74
Lucas Daniel <lucasdaniel.geo@gmail.com>
2024-02-16 12:07:37 -0300

    refact: get postgres env vars on connection function

--- internal/infra/env/main.go
+++ internal/infra/env/main.go
@@ -5,25 +5,9 @@ import (
 	"os"
 )
 
-func GetPostgresHost() string {
-	return os.Getenv("POSTGRES_HOST")
-}
-
-func GetPostgresDatabase() string {
-	return os.Getenv("POSTGRES_DB")
-}
-
-func GetPostgresUser() string {
-	return os.Getenv("POSTGRES_USER")
-}
-
-func GetPostgresPassword() string {
-	return os.Getenv("POSTGRES_PASSWORD")
-}
-
 func GetPostgresConnectionString() string {
 	return fmt.Sprintf(
-		"postgresql:%s:%s@%s/%s?sslmode=disable",
+		"postgresql://%s:%s@%s/%s?sslmode=disable",
 		os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PASSWORD"),
 		os.Getenv("POSTGRES_HOST"),
